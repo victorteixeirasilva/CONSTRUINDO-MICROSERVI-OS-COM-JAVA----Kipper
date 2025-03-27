3198dfeeca00a8a5de7e385d1c9934f55b3df981
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-27 19:43:42 -0300

    Gerando Micro Serviço de Eventos base com Java Spring

---
+++ events-microservice/src/main/java/com/victorteixeirasilva/events_microservice/EventsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.victorteixeirasilva.events_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ events-microservice/src/test/java/com/victorteixeirasilva/events_microservice/EventsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorteixeirasilva.events_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
